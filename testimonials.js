eced018f39625f728244dd57e60bdb53c312d8b3
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2018-12-31 23:21:04 +0700

    Create testimonials.js

---
+++ testimonials.js
@@ -0,0 +1,22 @@
+{
+  "testimonials": [
+    {
+      "name": "Ankur Thakur",
+      "position": "Managing Director",
+      "company": "Veridic Technologies Pvt Ltd",
+      "linkedInLink": "https://www.linkedin.com/in/ankur-thakur-20b31976/",
+      "website": "https://www.veridic.co.uk/",
+      "avatarUrl": "https://media.licdn.com/dms/image/C5103AQFaARngnMlwlg/profile-displayphoto-shrink_200_200/0?e=1551916800&v=beta&t=3TnyxBO5GDLEigZcwhx9XbLbIuYutJc6BHywIkt4Dvg",
+      "recommendation": "I have worked with Pradeep for several years on numerous projects. He was very driven and extremely passionate about his work. He always went out of the way to ensure that the best product was delivered and he had a knack to look at a problem from all angles before working out a solution. He is not only very intelligent, but also possesses great communication and customer facing skills due to which he adds immense value to any team he is working in. One of the most flexible people I have worked with who had the ability to take on any role given to him and deliver outstanding results in all of them."
+    },
+    {
+      "name": "A. Fabrizio Cicenia",
+      "position": "Sr. Director IT - LATAM",
+      "company": "Brightstar Corp.",
+      "linkedInLink": "https://www.linkedin.com/in/fabriziocicenia/",
+      "website": "https://www.brightstar.com/",
+      "avatarUrl": "https://media.licdn.com/dms/image/C4E03AQGftJ1S7L3YZQ/profile-displayphoto-shrink_800_800/0?e=1551916800&v=beta&t=gMholTDkGYcTk5f0N-54m6WYwqYcZgQF1ANgvsUz5iQ",
+      "recommendation": "Pradeep has shown an extremely high level of intellectual curiosity, independence and self-motivation throughout the time I have met him. He has always been very customer service oriented, and is very skilled at building rapport with customer. Additionally he is very well organized and possesses very strong administrative skills. In short, I give Pradeep my highest recommendation. He is one of the best consultants I have supervised and is a talented Project Manager, Developer and person as a whole. Pradeep is able to create and implement new projects very easily, and has an eager mind that seeks to understand everything he is doing. I strongly recommend him without reservation."
+    }
+  ]
+}
